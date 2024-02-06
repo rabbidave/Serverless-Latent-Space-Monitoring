8b0bc92dade027fca7f9f2194323e7e64b30043b
Nope Nope <d.isaac.pierce@gmail.com>
2024-02-06 16:13:11 -0600

    Refactored w/ Variables & Efficient Centroid Vector Caching
    Updated the terraform module to support use of variables.
    
    Updated the 2nd Lambda to support the following:
    
    1) Reading (and subsequently vectorizing) only items with the 'text' attribute from DynamoDB
    2) Retrieval of the most recent centroid vector (from S3) and comparison against net-new 'text' vectors; using a now cached value established upon initilization

--- Brain Blast.tf
+++ Brain Blast.tf
@@ -1,4 +1,13 @@
-# example resource policy to be applied to Lambda 2; still defining provisioning module for the DynamoDB instance and Lambda functions
+variable "region" {}
+variable "dynamodb_name" {}
+variable "environment_name" {}
+variable "vpc_id" {}
+variable "subnet_ids" {}
+variable "tags" {
+  default = {}
+}
+variable "s3_bucket_arn" {}
+
 provider "aws" {
   region = var.region
 }
@@ -6,10 +15,9 @@ provider "aws" {
 module "dynamodb_table" {
   source = "terraform-aws-modules/dynamodb-table/aws"
 
-  name           = var.dynamodb_name
-  read_capacity  = var.dynamodb_read_capacity
-  write_capacity = var.dynamodb_write_capacity
-  hash_key       = "id"
+  name         = var.dynamodb_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
 
   attributes = [
     {
@@ -21,42 +29,25 @@ module "dynamodb_table" {
   tags = merge(var.tags, { Name = var.dynamodb_name })
 }
 
-resource "aws_lambda_function" "jimmyneutron" {
-  filename         = "jimmyneutron.zip"
-  function_name    = "jimmyneutron"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "jimmyneutron.handler"
-  runtime          = "python3.8"
-  source_code_hash = filebase64sha256("jimmyneutron.zip")
-
-  environment {
-    variables = {
-      DYNAMODB_TABLE = module.dynamodb_table.this_table_name
-    }
-  }
-
-  vpc_config {
-    security_group_ids = [aws_security_group.sg.id]
-    subnet_ids         = var.subnet_ids
-  }
+resource "aws_sqs_queue" "alert_queue" {
+  name = "alert-queue-${var.environment_name}"
+  tags = merge(var.tags, { Name = "alert-queue-${var.environment_name}" })
 }
 
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = " lambda:InvokeFunction"
-  function_name = aws_lambda_function.jimmyneutron.function_name
-  principal     = "apigateway.amazonaws.com"
+resource "aws_sqs_queue" "deadletter_queue" {
+  name = "deadletter-queue-${var.environment_name}"
+  tags = merge(var.tags, { Name = "deadletter-queue-${var.environment_name}" })
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+  name = "iam_for_lambda_${var.environment_name}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -65,72 +56,40 @@ resource "aws_iam_role" "iam_for_lambda" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.iam_for_lambda.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_access_policy_${var.environment_name}"
+  role = aws_iam_role.iam_for_lambda.id
 
-resource "aws_sqs_queue" "alert_queue" {
-  name                       = "alert_queue"
-  delay_seconds              = 0
-  max_message_size            = 2048
-  message_retention_seconds   = 86400
-  receive_wait_time_seconds   = 0
-  visibility_timeout_seconds  = 0
-  enable_messages_delete_on_receive = false
-
-  redrive_policy = jsonencode({
-    deadLetterTargetArn = aws_sqs_queue.deadletter_queue.arn
-    maxReceiveCount     = 3
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "s3:PutObject",
+          "s3:GetObject",
+          "sqs:SendMessage",
+          "sqs:ReceiveMessage",
+          "comprehend:StartTopicsDetectionJob",
+          "comprehend:DescribeTopicsDetectionJob",
+        ],
+        Resource = [
+          module.dynamodb_table.this_table_arn,
+          var.s3_bucket_arn,
+          aws_sqs_queue.alert_queue.arn,
+          aws_sqs_queue.deadletter_queue.arn,
+        ]
+      },
+    ]
   })
-
-  tags = merge(var.tags, { Name = "alert_queue" })
-}
-
-resource "aws_sqs_queue" "deadletter_queue" {
-  name = "deadletter_queue"
-
-  tags = merge(var.tags, { Name = "deadletter_queue" })
-}
-
-resource "aws_lambda_event_source_mapping" "goddardcompute" {
-  event_source_arn  = module.dynamodb_table.this_table_arn
-  function_name     = aws_lambda_function.goddardcompute.function_name
-  starting_position = "LATEST"
-  enabled           = true
-}
-
-resource "aws_lambda_function" "goddardcompute" {
-  filename         = "goddardcompute.zip"
-  function_name    = "goddardcompute"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "goddardcompute.handler"
-  runtime          = "python3.8"
-  source_code_hash = filebase64sha256("goddardcompute.zip")
-
-  environment {
-    variables = {
-      ALERT_QUEUE_URL = aws_sqs_queue.alert_queue.id
-    }
-  }
-
-  vpc_config {
-    security_group_ids = [aws_security_group.sg.id]
-    subnet_ids         = var.subnet_ids
-  }
 }
 
 resource "aws_security_group" "sg" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
+  name        = "lambda-sg-${var.environment_name}"
+  description = "Security group for Lambda functions in ${var.environment_name}"
   vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
@@ -138,4 +97,53 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+# Note: The "module.lambda_functions" referenced below assumes a custom module that you need to define.
+# It should encapsulate the creation of Lambda functions, accepting configuration parameters for each function.
+module "lambda_functions" {
+  source = "./modules/lambda"
+
+  lambda_configs = [
+    {
+      name             = "jimmyneutron"
+      handler          = "jimmyneutron.handler"
+      filename         = "jimmyneutron.zip"
+      source_code_hash = filebase64sha256("jimmyneutron.zip")
+      environment_vars = {
+        DYNAMODB_TABLE = module.dynamodb_table.this_table_name
+      }
+      security_group_ids = [aws_security_group.sg.id]
+      subnet_ids         = var.subnet_ids
+    },
+    {
+      name             = "goddardcompute"
+      handler          = "goddardcompute.handler"
+      filename         = "goddardcompute.zip"
+      source_code_hash = filebase64sha256("goddardcompute.zip")
+      environment_vars = {
+        ALERT_QUEUE_URL = aws_sqs_queue.alert_queue.url
+      }
+      security_group_ids = [aws_security_group.sg.id]
+      subnet_ids         = var.subnet_ids
+    }
+  ]
+
+  role_arn = aws_iam_role.iam_for_lambda.arn
+}
+
+output "dynamodb_table_name" {
+  value = module.dynamodb_table.this_table_name
+}
+
+output "alert_queue_url" {
+  value = aws_sqs_queue.alert_queue.url
+}
+
+output "deadletter_queue_url" {
+  value = aws_sqs_queue.deadletter_queue.url
+}
+
+output "iam_for_lambda_arn" {
+  value = aws_iam_role.iam_for_lambda.arn
 }
--- Goddard, Compute!.py
+++ Goddard, Compute!.py
@@ -1,77 +1,81 @@
 import boto3
 from sentence_transformers import SentenceTransformer
 import numpy as np
-import datetime
+import json
+import os
 
 # Initializations
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('ClusterCentroids')
-pinecone.init()
+s3 = boto3.client('s3')
 sqs = boto3.client('sqs')
-ALERT_QUEUE_URL = 'your_sqs_queue_url'
+ALERT_QUEUE_URL = os.environ['ALERT_QUEUE_URL']
+S3_BUCKET = os.environ['S3_BUCKET_NAME']
 
 VECTOR_DIMENSION = 768
 THRESHOLD = 0.7 * np.sqrt(VECTOR_DIMENSION)
+encoder = SentenceTransformer('bert-base-nli-mean-tokens')
 
-def euclidean_distance(vec1, vec2):
-    return np.linalg.norm(np.array(vec1) - np.array(vec2))
+# Function to find the most recent centroid key based on timestamp
+def find_latest_centroid_key(bucket_name):
+    # Assumes centroid keys are stored by this function with a timestamp in their name, e.g., 'centroids/2023-01-01_centroid.json'
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix='centroids/')
+    centroid_files = response.get('Contents', [])
+    
+    if not centroid_files:
+        return None
+    
+    # Sort the files by LastModified date and return the key of the most recent
+    latest_file = max(centroid_files, key=lambda x: x['LastModified'])
+    return latest_file['Key']
 
-def get_centroid(attribute, value):
-    filter_condition = boto3.dynamodb.conditions.Attr(attribute).eq(value)
-    response = table.scan(FilterExpression=filter_condition)
-    if response['Items']:
-        return response['Items'][0]['vector']
-    return None
+# Updated function to load the most recent centroid from S3
+def load_centroid_from_s3():
+    global cached_centroid
+    centroid_key = find_latest_centroid_key(S3_BUCKET)
+    
+    if not centroid_key:
+        print("No centroid vector found in S3, initializing with zeros.")
+        cached_centroid = np.zeros(VECTOR_DIMENSION)
+        return
+    
+    try:
+        response = s3.get_object(Bucket=S3_BUCKET, Key=centroid_key)
+        centroid_data = json.loads(response['Body'].read().decode('utf-8'))
+        cached_centroid = np.array(centroid_data['vector'])
+    except Exception as e:
+        print(f"Error loading centroid from S3: {str(e)}")
+        cached_centroid = np.zeros(VECTOR_DIMENSION)
 
-def update_centroid(attribute, value, new_centroid):
-    cluster_id = f"cluster_1"
-    table.put_item(Item={
-        attribute: value,
-        'clusterID': cluster_id,
-        'vector': new_centroid,
-        'last_updated': str(datetime.datetime.now()),
-        'num_points': 1
-    })
+def update_centroid_in_s3(new_centroid):
+    # Generate a timestamped key for the new centroid
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    key = f"centroids/{timestamp}_centroid.json"
+    
+    s3.put_object(Bucket=S3_BUCKET, Key=key, Body=json.dumps({'vector': new_centroid.tolist()}))
+    print(f"Updated centroid vector stored as {key}")
 
 def lambda_handler(event, context):
-    encoder = SentenceTransformer('bert-base-nli-mean-tokens')
-    vectors = []
-    processed_records = []
-# Trigger only based on changes to DynamoDB; not deletion events
+    global cached_centroid
+    if cached_centroid is None:
+        load_centroid_from_s3()
+
     for record in event['Records']:
         if record['eventName'] in ['INSERT', 'MODIFY']:
             item = record['dynamodb']['NewImage']
             text = item['text']['S']
-            topics = item['topics']['S']
-            sentiment = item['sentiment']['S']
-            vector = encoder.encode([text, topics, sentiment])
-            vectors.append(vector)
-            uuid = item['id']['S']
-            pinecone.add_vector(uuid, vector)
-            processed_records.append(item)
-# For each new vector, compare against centroids for its sentiment, topic, and text; update centroid as needed
-    for item, vector in zip(processed_records, vectors):
-        sentiment = item['sentiment']['S']
-        topic = item['topics']['S']
-        text = item['text']['S']
-        attributes = [('sentiment', sentiment), ('topic', topic), ('text', text)]
-        
-        for attribute, value in attributes:
-            centroid = get_centroid(attribute, value)
-            if centroid:
-                distance = euclidean_distance(vector, centroid)
-                if distance > THRESHOLD:
-                    new_centroid = (np.array(centroid) + np.array(vector)) / 2
-                    update_centroid(attribute, value, new_centroid.tolist())
-                    alert_message = {
-                        'text': text,
-                        'topics': topic,
-                        'sentiment': sentiment
-                    }
-                    sqs.send_message(QueueUrl=ALERT_QUEUE_URL, MessageBody=str(alert_message))
-            else:
-                update_centroid(attribute, value, vector.tolist())
-    
+            vector = encoder.encode([text])
+
+            distance = euclidean_distance(vector, cached_centroid)
+            if distance > THRESHOLD:
+                cached_centroid = (cached_centroid + vector) / 2
+                update_centroid_in_s3(cached_centroid)
+                alert_message = {
+                    'text': text,
+                    'distance': distance,
+                    'threshold_exceeded': True
+                }
+                sqs.send_message(QueueUrl=ALERT_QUEUE_URL, MessageBody=json.dumps(alert_message))
+
     return {
         'statusCode': 200,
         'body': 'Processed successfully'
